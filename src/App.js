64132da3c1987a2b175d6443de0ee955da951ab4
Brianna Gordon <brigordon574@gmail.com>
2021-06-22 09:31:28 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import react from "react";
+import Login from "./Login";
+import Services from "./Services"
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <h1>The Hair Code</h1>
+      </header>
+      {/* <Login  />
+      <Services /> */}
+    </div>
+  );
+}
+
+export default App;
