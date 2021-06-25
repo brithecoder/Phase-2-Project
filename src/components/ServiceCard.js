8b716ff33e78c8e63f932d9890f94088b5cf3d14
Nahum <losond8@gmail.com>
2021-06-24 17:57:06 -0700

    Fixed code on main.

--- src/components/ServiceCard.js
+++ src/components/ServiceCard.js
@@ -1,20 +1,3 @@
-<<<<<<< HEAD
-import React, { useState } from "react";
-import { Button, Card } from 'semantic-ui-react';
-import Calendar from "../components/Calendar";
-
-
-
-function ServiceCard({service}){
-    console.log(service)
-    
-    // function handleClick(){
-    //     calendarFunction(service);
-    // }
-
-    return(
-    
-=======
 import React from "react";
 
 
@@ -29,7 +12,6 @@ function ServiceCard({service, fromSignIn}){
    // console.log(service)
 
     return (
->>>>>>> main
         <div class="service-card">
             <div class="ui special cards">
                 <div class="card">
@@ -37,70 +19,28 @@ function ServiceCard({service, fromSignIn}){
                         <div class="ui dimmer">
                             <div class="content">
                                 <div class="center">
-                                <div class="ui inverted button">Add Friend</div>
+                                    <div class="ui inverted button">Add Friend</div>
+                                </div>
                             </div>
                         </div>
+                        <img src={service.img_url} alt={service.name}/>
                     </div>
-                    <img src={service.img_url} alt={service.name}/>
-                </div>
-                <div class="content">
-                    <a href="./" class="header">{service.name} </a>
-                    <div class="meta">
-                    <span class="date">{service.description}</span>
-                </div>
-            </div>
-            <div class="extra content">
-                <h5>
-                    <i class="users icon"></i>Price: ${service.price} 
-                    <button onClick={handleBook} class="ui right floated button">Book Now</button>
-                    <br/> Duration: {service.duration} 
-                </h5>
-            </div>
-            {/* <div class="extra content">
-                <a> <i class="users icon"></i>  2 Members </a>
-            </div>
-            </div>
-                <div class="card">
-                    <div class="blurring dimmable image">
-                        <div class="ui inverted dimmer">
-                            <div class="content">
-                                <div class="center">
-                                <div class="ui primary button">Add Friend</div>
-                            </div>
+                    <div class="content">
+                        <a href="./" class="header">{service.name} </a>
+                        <div class="meta">
+                            <span class="date">{service.description}</span>
                         </div>
                     </div>
-                    <img src="/images/avatar/large/jenny.jpg"/> 
-                </div>
-                <div class="content">
-                    <a class="header">Team Hess</a>
-                    <div class="meta">
-                        <span class="date">Created in Aug 2014</span>
+                    <div class="extra content">
+                        <h5>
+                            <i class="users icon"></i>Price: ${service.price} 
+                            <button onClick={handleBook} class="ui right floated button">Book Now</button>
+                            <br/> Duration: {service.duration} 
+                        </h5>
                     </div>
-                </div>
-                <div class="extra content">
-                    <a href="./">
-                    <i class="users icon"></i>
-                     price: ${service.price} 
-                      <br/> Duration: {service.duration}
-                    </a>
-<<<<<<< HEAD
-                </div>
-                 
-                   Book Now<Calendar />
-            </div>
-        </div>         
-    </div> 
-    );
-}
-export default ServiceCard;
-
-
-=======
-                </div> */}
                 </div>
             </div>         
         </div> 
     );
 }
 export default ServiceCard;
->>>>>>> main
