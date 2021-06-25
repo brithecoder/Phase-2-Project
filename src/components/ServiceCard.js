2551a956c0c90c8ff0124a83ea37d8248673b469
Nahum <losond8@gmail.com>
2021-06-24 17:34:01 -0700

    Updating component for merge conflict.

--- src/components/ServiceCard.js
+++ src/components/ServiceCard.js
@@ -19,53 +19,25 @@ function ServiceCard({service, fromSignIn}){
                         <div class="ui dimmer">
                             <div class="content">
                                 <div class="center">
-                                <div class="ui inverted button">Add Friend</div>
+                                    <div class="ui inverted button">Add Friend</div>
+                                </div>
                             </div>
                         </div>
+                        <img src={service.img_url} alt={service.name}/>
                     </div>
-                    <img src={service.img_url} alt={service.name}/>
-                </div>
-                <div class="content">
-                    <a href="./" class="header">{service.name} </a>
-                    <div class="meta">
-                    <span class="date">{service.description}</span>
-                </div>
-            </div>
-            <div class="extra content">
-                <h5>
-                    <i class="users icon"></i>Price: ${service.price} 
-                    <button onClick={handleBook} class="ui right floated button">Book Now</button>
-                    <br/> Duration: {service.duration} 
-                </h5>
-            </div>
-            {/* <div class="extra content">
-                <a> <i class="users icon"></i>  2 Members </a>
-            </div>
-            </div>
-                <div class="card">
-                    <div class="blurring dimmable image">
-                        <div class="ui inverted dimmer">
-                            <div class="content">
-                                <div class="center">
-                                <div class="ui primary button">Add Friend</div>
-                            </div>
+                    <div class="content">
+                        <a href="./" class="header">{service.name} </a>
+                        <div class="meta">
+                            <span class="date">{service.description}</span>
                         </div>
                     </div>
-                    <img src="/images/avatar/large/jenny.jpg"/> 
-                </div>
-                <div class="content">
-                    <a class="header">Team Hess</a>
-                    <div class="meta">
-                        <span class="date">Created in Aug 2014</span>
+                    <div class="extra content">
+                        <h5>
+                            <i class="users icon"></i>Price: ${service.price} 
+                            <button onClick={handleBook} class="ui right floated button">Book Now</button>
+                            <br/> Duration: {service.duration} 
+                        </h5>
                     </div>
-                </div>
-                <div class="extra content">
-                    <a href="./">
-                    <i class="users icon"></i>
-                     price: ${service.price} 
-                      <br/> Duration: {service.duration}
-                    </a>
-                </div> */}
                 </div>
             </div>         
         </div> 
